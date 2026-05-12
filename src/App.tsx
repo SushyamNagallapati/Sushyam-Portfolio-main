0da588656c248d29bb9b83ad58858a939c2c9873
Sushyam Nagallapati <sushyamraju@gmail.com>
2026-05-12 16:45:54 -0400

    Remove unused CSS styles and clean up routing comments in App component

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,6 @@ const App = () => (
             <Route path="/" element={<Index />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/resume" element={<Resume />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
